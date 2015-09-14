e3d48b123c297fc57361a6cca1217ad7bbca0bb1
Leo Zhou <glasslion@gmail.com>
2015-09-14 22:42:58 +0800

    dummy completer

--- saltcli/main.py
+++ saltcli/main.py
@@ -1,8 +1,11 @@
 #!/usr/bin/env python
 # -*- coding: utf-8
 from __future__ import print_function
+import logging
 
 import click
+from prompt_toolkit.contrib.completers import WordCompleter
+from prompt_toolkit.shortcuts import get_input
 
 from .__init__ import __version__
 
@@ -11,6 +14,12 @@ class SaltCli(object):
     """
     The CLI implementation.
     """
+    def __init__(self):
+        self.logger = logging.getLogger(__name__)
+        self.id_completer = WordCompleter([
+            'id1', 'id2', 'id3'
+        ])
+
 
     def run_cli(self):
         """
@@ -18,6 +27,11 @@ class SaltCli(object):
         """
         print(u'Version:', __version__)
         print(u'Home: https://github.com/glasslion/saltcli')
+
+        while True:
+            text = get_input(u"> ", completer=self.id_completer)
+
+            print(u'You entered:', text)
 
 
 @click.command()

703c574ff03a0ff5e152044bfefeeed78f7483eb
Leo Zhou <glasslion@gmail.com>
2015-09-22 22:09:32 +0800

    A dummy impl with application layout and eventloop

--- saltcli/main.py
+++ saltcli/main.py
@@ -2,10 +2,13 @@
 # -*- coding: utf-8
 from __future__ import print_function
 import logging
+import os.path
 
 import click
+from prompt_toolkit import CommandLineInterface, Application, AbortAction
 from prompt_toolkit.contrib.completers import WordCompleter
-from prompt_toolkit.shortcuts import get_input
+from prompt_toolkit.history import FileHistory
+from prompt_toolkit.shortcuts import create_prompt_layout, create_eventloop
 
 from .__init__ import __version__
 
@@ -28,12 +31,44 @@ class SaltCli(object):
         print(u'Version:', __version__)
         print(u'Home: https://github.com/glasslion/saltcli')
 
+        history = FileHistory(os.path.expanduser('~/.saltcli-history'))
+
+        layout = create_prompt_layout(
+            message=u'saltcli> ',
+        )
+
+        application = Application(
+            layout=layout
+        )
+
+        eventloop = create_eventloop()
+
+        self.cli = CommandLineInterface(
+            application=application,
+            eventloop=eventloop)
+
         while True:
-            text = get_input(u"> ", completer=self.id_completer)
+            try:
+                document = self.cli.run()
+            except OptionError as ex:
+                self.logger.debug('Error: %r.', ex)
+                self.logger.error("traceback: %r", traceback.format_exc())
+                click.secho(ex.msg, fg='red')
+            except DockerPermissionException as ex:
+                self.logger.debug('Permission exception: %r.', ex)
+                self.logger.error("traceback: %r", traceback.format_exc())
+                click.secho(ex.message, fg='red')
+            except EOFError:
+                break
+            except Exception as ex:
+                self.logger.debug('Exception: %r.', ex)
+                self.logger.error("traceback: %r", traceback.format_exc())
+                click.secho("{0}".format(ex), fg='red')
+                break
 
-            print(u'You entered:', text)
-
-
+        self.revert_less_opts()
+        self.write_config_file()
+        print('Goodbye!')
 @click.command()
 def cli():
     """

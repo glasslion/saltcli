3cd5bf4cbf26422c94cfa1518d63778539605295
Leo Zhou <glasslion@gmail.com>
2016-01-25 10:04:09 +0800

    Use a custom docker start script(remove sudo, add pip install -e)

---
+++ docker_start.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# Variables from environement
+: "${SALT_USE:=master}"
+: "${SALT_NAME:=master}"
+: "${LOG_LEVEL:=info}"
+: "${OPTIONS:=}"
+
+# Set minion id
+echo $SALT_NAME > /etc/salt/minion_id
+
+# If salt master also start minion in background
+if [ $SALT_USE == "master" ]; then
+  echo "INFO: Starting salt-minion and auto connect to salt-master"
+  service salt-minion start
+fi
+
+# Set salt grains
+if [ ! -z "$SALT_GRAINS" ]; then
+  echo "INFO: Set grains on $SALT_NAME to: $SALT_GRAINS"
+  echo $SALT_GRAINS > /etc/salt/grains
+fi
+
+# pip install saltcli as dev
+pip install -e /src/saltcli/
+
+# Start salt-$SALT_USE
+echo "INFO: Starting salt-$SALT_USE with log level $LOG_LEVEL with hostname $SALT_NAME"
+/usr/bin/salt-$SALT_USE --log-level=$LOG_LEVEL $OPTIONS
